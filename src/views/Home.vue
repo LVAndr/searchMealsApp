a231ed0c5d4df067152b7df449e12964a0ce0916
vadim <vadim.lylko@gmail.com>
2024-06-29 19:58:07 +0300

    install Vuex and create store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <script setup>
+import {computed} from "vue";
+import store from "../store/index.js";
 
+const meals = computed(()=> store.state.meals)
 </script>
 <template>
-  Home
+  <pre>{{meals}}</pre>
 </template>
 <style>
 
