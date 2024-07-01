a91a77561d6d7c64efa0893597ff61b261c973f7
vadim <vadim.lylko@gmail.com>
2024-07-01 22:05:18 +0300

    add DefaultLayout, NavBar components, refactoring vue-router

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import NavBar from "./NavBar.vue";
+</script>
+
+<template>
+<div class="bg-gray-100 h-full">
+  <nav-bar/>
+  <main>
+    <router-view/>
+  </main>
+</div>
+</template>
+
+<style scoped>
+
+</style>
