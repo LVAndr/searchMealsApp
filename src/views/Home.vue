b3b816ed97525217c06e3474a9225a1b97664937
vadim <vadim.lylko@gmail.com>
2024-07-02 22:41:05 +0300

    add searchMeals fn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,12 +15,6 @@ onMounted(async ()=>{
 </script>
 <template>
   <div class="flex flex-col p-8">
-      <input
-          type="text" name="" id=""
-          class="rounded border-2 border-gray-200 w-full"
-          placeholder="Search for meals"
-      />
-
 
     <div class="flex justify-center gap-2 mt-2">
       <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,11 +1,29 @@
-MealList.vue<script setup>
+<script setup>
+import {computed, ref} from "vue";
+import store from "../store/index.js";
 
+const keyword = ref('');
+const meals = computed(()=> store.state.searchedMeals);
+
+function searchMeals(){
+  store.dispatch('searchMeals', keyword.value);
+}
 </script>
 
 <template>
-<div>
- By Name
+<div class="p-8">
+  <input
+      type="text" name="" id=""
+      v-model="keyword"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for meals"
+      @change="searchMeals"
+  />
 </div>
+
+  <div>
+    <pre>{{meals}}</pre>
+  </div>
 </template>
 
 <style scoped>
